f26d10148c7db029d743e2f6bcb24e04c0c97799
Ro <53170106+dtkieXRo@users.noreply.github.com>
2025-07-13 20:24:04 +0700

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -4,7 +4,7 @@
 // @version      1.0-old
 // @description  Bypass Funlink
 // @author       Cac No Name
-// @require      https://raw.githubusercontent.com/dtkieXRo/bypass-funlink.io/main/tampermonkey.user.js
+// @require      https://raw.githubusercontent.com/dtkieXRo/bypass-funlink.io/refs/heads/main/bypass_funlink.js
 // @match        https://funlink.io/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
